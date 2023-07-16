bf3707d5d783c617c859246a541fed491c5b89c2
kanon chakma <kanonchakma@pop-os.localdomain>
2023-07-16 22:25:00 +0600

    add sidebar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,6 +1,14 @@
-import { AppBar, Toolbar, useTheme } from '@mui/material'
-import React from 'react'
-import { useDispatch } from 'react-redux'
+import {
+  DarkModeOutlined, LightModeOutlined, Menu as MenuIcon,
+  Search,
+  SettingsOutlined
+} from "@mui/icons-material";
+import { AppBar, IconButton, InputBase, Toolbar, useTheme } from '@mui/material';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setMode } from 'state';
+import FlexBetween from './FlexBetween';
+
 
 const NavBar = () => {
   const dispatch = useDispatch()
@@ -14,9 +22,43 @@ const NavBar = () => {
       boxShadow: "none"
     }}
     >
-      <Toolbar>
+      <Toolbar sx={{ justifyContent: "space-between"}}>
+        
+        <FlexBetween onClick={() => console.log('open/close slidebar')}>
+           <IconButton>
+             <MenuIcon/>
+           </IconButton>
+           
+           <FlexBetween
+             backgroundColor = {theme.palette.background.alt}
+             borderRadius="9px"
+             gap="3rem"
+             padding="0.1rem 1.5rem"
+           >
+              <InputBase placeholder='Search..'/>
+              <IconButton>
+               <Search/>
+              </IconButton>
+           </FlexBetween>
+        </FlexBetween>
+
+        <FlexBetween gap="1.5rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {
+              theme.palette.mode === "dark"?(
+                <DarkModeOutlined sx={{fontSize: "25px"}}/>
+              ):(
+                <LightModeOutlined sx={{fontSize: "25px"}}/>
+              )
+            }
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{fontSize: "25px"}}/>
+          </IconButton>
+         
+        </FlexBetween>
       
-      </Toolbar>
+        </Toolbar>
     </AppBar>
   )
 }
--- client/src/scenes/Layout/index.js
+++ client/src/scenes/Layout/index.js
@@ -1,13 +1,23 @@
-import { Box } from '@mui/material'
+import { Box, useMediaQuery } from '@mui/material'
 import NavBar from 'components/NavBar'
-import React from 'react'
+import SideBar from 'components/SideBar'
+import React, { useState } from 'react'
 import { Outlet } from 'react-router-dom'
 
 const LayOut = () => {
+  const isNoneMobile = useMediaQuery('(min-width:600px)');
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+
   return (
-    <Box width="100%" height="100%">
+    <Box display={isNoneMobile ? "flex": "block"} width="100%" height="100%">
+      <SideBar
+      isNoneMobile={isNoneMobile}
+      drawerWidth="250px"
+      isSidebarOpen={isSidebarOpen}
+      setIsSidebarOpen={setIsSidebarOpen}
+      />
       <Box>
-       <NavBar/>
+       <NavBar isSidebarOpen={isSidebarOpen} setIsSidebarOpen={setIsSidebarOpen}/>
        <Outlet/>
       </Box>
     </Box>
